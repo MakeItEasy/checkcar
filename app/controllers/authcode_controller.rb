eaef9848cb26caa32c656e611e2ff99187e53b9c
MakeItEasy <rugang6891@gmail.com>
2014-07-04 16:10:19 +0800

    手机动态验证码发送controller实现

---
+++ app/controllers/authcode_controller.rb
@@ -0,0 +1,34 @@
+### 手机动态验证码
+class AuthcodeController < ActionController::Base
+
+  # POST /phone_autucode
+  # POST /admins.json
+  def phone_authcode
+    phone = params[:phone]
+    captcha = params[:captcha]
+    errors = {}
+    if phone.blank?
+      errors[:phone] = "手机号必需"
+    elsif User.find_by(telephone: phone).blank?
+      errors[:phone] = "该手机号未注册"
+    end
+    if captcha.blank?
+      errors[:captcha] = "验证码必需"
+    elsif !valid_captcha?(captcha)
+      errors[:captcha] = "验证码错误"
+    end
+
+    respond_to do |format|
+      if errors.blank?
+        # 发送动态码
+        # TODO dairg 发送动态码 这里不要用session，因为还要记录发送时间
+        session[:phone_authcode] = '1111'
+        session[:phone_authcode_send_time] = Time.new
+        format.json { render json: {}}
+      else
+        format.json { render json: {errors: errors}}
+        #format.json { render json: {}}
+      end
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,11 @@ Rails.application.routes.draw do
     get "users/sign_in_by_telephone" => "users/sessions#new_by_telephone", as: 'new_by_telephone_user_session'
   end
 
+  # 手机动态验证码
+  post "phone_authcode"  => "authcode"
+  # TODO dairg delete
+  get "phone_authcode"  => "authcode"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
