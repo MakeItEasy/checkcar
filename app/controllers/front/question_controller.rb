1fdf2f791652aa22561020c554de47cd661a667a
MakeItEasy <rugang6891@gmail.com>
2014-07-08 17:38:27 +0800

    常见问题前台翻页处理

--- app/controllers/front/question_controller.rb
+++ app/controllers/front/question_controller.rb
@@ -3,6 +3,12 @@ class Front::QuestionController < FrontController
 
   ## 常见问题
   def index_faq
+    @faqs = Faq
+    if params[:name_faq]
+      _like_name_faq = "%#{params[:name_faq]}%"
+      @faqs = @faqs.where("question like ? or answer like ?", _like_name_faq, _like_name_faq)
+    end
+    @faqs = @faqs.page(params[:page]).per(10)
   end
 
   ## 用户问题
