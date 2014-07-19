00afd46cff07f00501f9d12a7b3ae097aa0bdc68
MakeItEasy <rugang6891@gmail.com>
2014-07-19 17:43:28 +0800

    页面列表的微微调整，时间标记的添加

--- app/controllers/front/welcome_controller.rb
+++ app/controllers/front/welcome_controller.rb
@@ -20,32 +20,7 @@ class Front::WelcomeController < FrontController
     @next_post = @post.next_published_post
   end
 
-  ## 预约
-  # TODO dairg 暂时放到这里
-  def order
-    @steps = steps
-    @current_step = steps.first
-    session[:current_step] = @current_step
-  end
-
-  def step
-    @steps = steps
-    @current_step = session[:current_step]
-    if params[:type] == 'next'
-      @current_step = steps[steps.index(@current_step)+1] || steps.first
-    elsif params[:type] == 'pre'
-      @current_step = steps[steps.index(@current_step)-1]
-    end
-    session[:current_step] = @current_step
-
-    render :order
-  end
-
 private
-  def steps
-    %w[select_station select_date basic_info confirmation success]
-  end
-
   def set_catagory
     @catagory = Catagory.find(params[:catagory_id])
   end
