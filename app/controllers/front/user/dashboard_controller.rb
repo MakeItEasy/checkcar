18cd9ef9bfed5faca565e5d5c0090d9e36a2d918
MakeItEasy <rugang6891@gmail.com>
2014-08-02 16:30:05 +0800

    front dashboard显示可进行预约

--- app/controllers/front/user/dashboard_controller.rb
+++ app/controllers/front/user/dashboard_controller.rb
@@ -3,5 +3,7 @@ class Front::User::DashboardController < Front::UserBaseController
   add_breadcrumb I18n.t('view.controller.dashboard'), :front_user_root_path
 
   def index
+    # 进行中预约
+    @orders = current_user.orders.success.accessible_by(current_ability)
   end
 end
