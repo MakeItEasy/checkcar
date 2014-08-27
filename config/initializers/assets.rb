1c77afb7125ca083bafdd78bb101c06f5a12c36a
MakeItEasy <rugang6891@gmail.com>
2014-08-27 15:40:46 +0800

    IE初步对应

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -2,6 +2,7 @@ Rails.application.config.assets.precompile += %w( front.css front.js)
 Rails.application.config.assets.precompile += %w( back.css back.js)
 Rails.application.config.assets.precompile += %w( devise_admin.css)
 Rails.application.config.assets.precompile += %w( devise_user.css devise_user.js)
+Rails.application.config.assets.precompile += %w( html5shiv.js respond.js css3-mediaqueries.js)
 =begin
 Rails.application.config.assets.precompile += %w( ckeditor/filebrowser/javascripts/*.js )
 Rails.application.config.assets.precompile += %w( ckeditor/filebrowser/stylesheets/*.css )
@@ -10,6 +11,9 @@ Rails.application.config.assets.precompile += %w( ckeditor/lang/*.js )
 =end
 Rails.application.config.assets.precompile += %w( fonts/*.*)
 Rails.application.config.assets.precompile += %w( icons/grid/*.*)
+
+# bsie
+Rails.application.config.assets.precompile += %w( bsie/bootstrap-ie6.css bsie/ie.css bsie/bootstrap-ie.js)
 
 # 百度地图
 Rails.application.config.assets.precompile += %w( baidu_map/SearchInfoWindow.js)
