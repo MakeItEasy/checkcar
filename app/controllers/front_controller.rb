eb5d5d79cefd143f1fb2bad8fdeacfe9e5f2a2e7
MakeItEasy <rugang6891@gmail.com>
2014-09-13 22:24:31 +0800

    切换城市样式修改，车检站行政区域部分根据当前城市进行适配显示

--- app/controllers/front_controller.rb
+++ app/controllers/front_controller.rb
@@ -20,36 +20,33 @@ private
     @catagories = Catagory.all
   end
 
+  # 子域名是否有效，必须是已经开通的城市的拼音
   def subdomain_ok?
     request.subdomain.present? && request.subdomain != 'www' &&
       request.subdomains.size == 1 && OpenCity.is_valid_subdomain?(request.subdomain)
   end
 
+  # 默认的城市定位
   def default_current_city
     # TODO dairg IP查找
     # TODO dairg default current city
     OpenCity.find(1)
   end
 
+  # 当前城市
   def current_open_city
     @current_open_city || OpenCity.find_by_short_name(request.subdomain) || 
       default_current_city
   end
 
-
+  # 设置城市或者切换城市
   def set_current_city
-    puts "====================="
-    puts cookies[:current_city]
-    # cookies.delete :current_city
     if subdomain_ok?
-      puts "11111111111111111111"
       cookies[:current_city] = {value: request.subdomain, domain: 'car.me'}
       @current_open_city = OpenCity.find_by_short_name(request.subdomain)
     else
-      puts "2222222222222222222222"
       _open_city = OpenCity.find_by_short_name(cookies[:current_city])
       if _open_city.nil?
-        puts "333333333333333333333333"
         _open_city = default_current_city
       end
       @current_open_city = _open_city
